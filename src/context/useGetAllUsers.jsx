4f9d766767d860348433d2c6f0a92fb218df0012
AmarJeetVerma9151 <vermaamarjeet095@gmail.com>
2025-07-10 17:02:29 +0530

    jwt

--- src/context/useGetAllUsers.jsx
+++ src/context/useGetAllUsers.jsx
@@ -8,13 +8,10 @@ function useGetAllUsers() {
     const getUsers = async () => {
       setLoading(true);
       try {
-        const token = Cookies.get("jwt");
-        const response = await axios.get("https://chatapp-qlz2.onrender.com/api/user/allusers", {
-          credentials: "include",
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
+        // const token = Cookies.get("jwt");
+       const response = await axios.get("https://chatapp-qlz2.onrender.com/api/user/allusers", {
+  withCredentials: true, // ✅ correct syntax for Axios
+});
         setAllUsers(response.data);
         setLoading(false);
       } catch (error) {
